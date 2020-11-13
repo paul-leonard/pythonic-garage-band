113bd9d565cebf70216d3f76cfc2fb1a7eeef5a1
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-12 18:27:27 -0800

    added README, base template files, and passing first guitarist test

---
+++ README.md
@@ -0,0 +1,25 @@
+# pythonic-garage-band
+Want to be in a band!?! We are setting one up with this sweet Python OOP exercise! Grab an instrument and look to your class for instructions.
+
+## Lab Submission Pull Requests
+[Lab04: Garage Band with OOP](https://github.com/paul-leonard/pythonic-garage-band/pull/1)
+
+## Release Info
+**Author**: Paul Leonard
+**Version**: 0.9.0
+
+## Overview
+This python script uses Object Oriented Programming methodoliges to structure a fictional band using classes and objects to layout the structure and composition of the band and their talents.
+
+## Architecture
+TBD
+
+## Change Log
+**0.9.0** 11-12-2020 - Initial beta release
+**1.0.0** 11-TBD-2020 - Initial release
+
+## Credits and Collaborations
+- Thanks to the SoloLearn Python Course for teaching me the basics to use in this app
+- Thanks to the team at Code Fellows Team!
+- Thanks to classmates I discussed and conferred with:  Yoni, Taylor, Lee
+- Additional thanks to Skyler for some pointers I heard them giving out.
---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,52 @@
+'''Instructions and Features:
+Use Python classes to model a Band made up of different kinds of musicians.
+
+Start with Guitarist,Bassist, and Drummer.
+
+Make use of a Musician base class to handle common functionality which particular kinds of musicians will inherit.
+
+User Acceptance Tests
+Unit tests will be supplied for this lab. Your job is to make them pass. Do NOT modify the supplied tests (except to enable for stretch goals.)
+
+Band Tests
+A Band instance should have a name attribute which is a string.
+A Band instance should have a members attribute which is a list of instances that inherit from Musician base (or super) class.
+A Band instance should have a play_solos method that asks each member musician to play a solo, in the order they were added to band.
+A Band instance should have appropriate __str__ and __repr__ methods.
+A Band should have a class method to_list which returns a list of previously created Band instances
+Musician Subclass Tests
+Each kind of Musician instance should have appropriate __str__ and __repr__ methods.
+Each kind of Musician instance should have a get_instrument method that returns string.
+Each kind of Musician instance should have a play_solo method that returns string.
+'''
+
+
+class Band:
+    def __init__(self, name, members=None):
+        self.name = name
+        self.members = members
+
+    def __str__(self):
+        return f"The band {self.name}"
+
+    def __repr__(self):
+        return f"Band instance. name={self.name}, members={self.members}"
+
+
+class Musician:
+    pass
+
+class Guitarist:
+  #pass
+  def __init__(self, name):
+    self.instrument = "guitar"
+    self.name = name
+
+  def __str__(self):
+    return f"My name is {self.name} and I play {self.instrument}"
+
+class Bassist:
+    pass
+
+class Drummer:
+    pass
